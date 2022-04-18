1e4af71f197af7e319920d6c1892e824184fa158
Arzl James <arzljames15@gmail.com>
2022-04-18 22:38:22 +0800

    sds

--- Routes/ReportRoute.js
+++ Routes/ReportRoute.js
@@ -17,15 +17,15 @@ router.post("/create", async (req, res) => {
   const report = {};
   try {
     let result = await Report.create({
-      creator: req.body.creator,
-      reportId: req.body.reportId,
-      from: req.body.from,
-      to: req.body.to,
-      refer: req.body.refer,
-      specialization: req.body.specialization,
-      age: req.body.age,
-      gender: req.body.gender,
-      physician: req.body.physician,
+      creator: !req.body.creator ? "" : req.body.creator,
+      reportId: !req.body.reportId ? "" : req.body.reportId,
+      from: !req.body.from ? "" : req.body.from,
+      to: !req.body.to ? "" : req.body.to,
+      refer: !req.body.refer ? "" : req.body.refer,
+      specialization: !req.body.specialization ? "" : req.body.specialization,
+      age: !req.body.age ? "" : req.body.age,
+      gender: !req.body.gender ? "" : req.body.gender,
+      physician: !req.body.physician ? "" : req.body.physician,
     });
 
     if (result) {
