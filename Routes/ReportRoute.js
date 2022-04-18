d7477a7cee098460083e1f6d042906b5dd33d446
Arzl James <arzljames15@gmail.com>
2022-04-18 16:12:01 +0800

    sdS

--- Routes/ReportRoute.js
+++ Routes/ReportRoute.js
@@ -1,9 +1,25 @@
 const router = require("express").Router();
+const Report = require("../Models/Report");
 
+router.post("/create", async (req, res) => {
+  const report = {
+    creator: req.body.creator,
+    reportId: req.body.reportId,
+    from: req.body.from,
+    to: req.body.to,
+    refer: req.body.refer,
+    specialization: req.body.specialization,
+    age: req.body.age,
+    gender: req.body.gender,
+    physician: req.body.physician,
+  };
+  try {
+    let result = await Report.create(report);
 
-router.get("/", (req, res) => {
-    res.send("Report")
-})
-
+    if (result) {
+      res.send(result);
+    }
+  } catch (error) {}
+});
 
 module.exports = router;
