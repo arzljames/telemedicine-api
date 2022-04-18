697647060c574d7639cc11141751f44c130e0936
Arzl James <arzljames15@gmail.com>
2022-04-18 22:14:29 +0800

    Sds

--- Models/Report.js
+++ Models/Report.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const User = require("../Models/User");
+const Facilities = require("../Models/Facilities");
 
 const ReportSchema = new mongoose.Schema(
   {
@@ -10,11 +11,17 @@ const ReportSchema = new mongoose.Schema(
     reportId: String,
     from: Date,
     to: Date,
-    refer: String,
+    refer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: Facilities,
+    },
     specialization: String,
     age: Number,
     gender: String,
-    physician: String,
+    physician: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: User,
+    },
   },
   { timestamps: true }
 );
