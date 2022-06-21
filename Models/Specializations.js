7fec90a7d136acfa14f6db270f687f7655d4c574
Arzl James <70579069+arzljames@users.noreply.github.com>
2022-06-21 15:03:03 +0800

    sdS

---
+++ Models/Specializations.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const User = require("../Models/User");
+
+const SpecializationSchema = new mongoose.Schema(
+  {
+    specialization: String,
+    user: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: User,
+      },
+    ],
+  },
+
+  { timestamps: true }
+);
+
+const Specialization = mongoose.model(
+  "Specialization",
+  SpecializationSchema,
+  "Specialization"
+);
+module.exports = Specialization;
