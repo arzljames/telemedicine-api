5ff754a8392cfde7a70f846e6a107d7140afc885
Arzl James <70579069+arzljames@users.noreply.github.com>
2022-06-22 10:31:03 +0800

    Sds

--- Routes/SpecializationRoute.js
+++ Routes/SpecializationRoute.js
@@ -1,27 +1,34 @@
 const router = require("express").Router();
-const Specialization = require("../Models/Specialization")
+const Specialization = require("../Models/Specialization");
 
+router.post("/add", async (req, res) => {
+  const specialization = req.body.specialization;
+  const description = req.body.description;
 
+  try {
+    let result = await Specialization.create({
+      specialization,
+      description,
+    });
 
-
-router.post("/add", async(req, res) => {
-    const specialization = req.body.specialization;
-    const description = req.body.description;
-
-
-    try {
-        let result = await Specialization.create({
-            specialization,
-            description
-        })
-
-        if(result) {
-            res.send({ok: "Added Specialization"})
-        }
-    } catch (error) {
-        console.log(error)
+    if (result) {
+      res.send({ ok: "Added Specialization" });
     }
-})
+  } catch (error) {
+    console.log(error);
+  }
+});
 
+router.get("/", async (req, res) => {
+  try {
+    let result = await Specialization.find({});
+
+    if (result) {
+      res.send(result);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;
