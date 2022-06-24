9ab1f1a0fe7a05a280d0794b6300ff3a43a52c16
Arzl James <70579069+arzljames@users.noreply.github.com>
2022-06-24 10:41:12 +0800

    sds

--- Models/Patient.js
+++ Models/Patient.js
@@ -1,84 +1,82 @@
 const mongoose = require("mongoose");
 const User = require("../Models/User");
 
-const PatientSchema = new mongoose.Schema({
-  firstname: { type: String, required: true, trim: true },
+const PatientSchema = new mongoose.Schema(
+  {
+    firstname: { type: String, required: true, trim: true },
 
-  middlename: {
-    type: String,
-    required: String,
-    trim: true,
-  },
-  lastname: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  fullname: {
-    type: String,
-  },
-  contact: {
-    type: Number,
-    required: true,
-  },
-  sex: {
-    type: String,
-    required: true,
-  },
-  birthday: {
-    type: String,
-    required: true,
-  },
-  civilStatus: {
-    type: String,
-    required: true,
-  },
-  religion: {
-    type: String,
-    required: true,
-  },
-  birthplace: {
-    type: String,
-    required: true,
-  },
-  address: {
-    street: {
+    middlename: {
+      type: String,
+      required: String,
+      trim: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+    },
+    contact: {
+      type: Number,
+      required: true,
+    },
+    sex: {
       type: String,
       required: true,
     },
-    barangay: {
+    birthday: {
       type: String,
       required: true,
     },
-    city: {
+    civilStatus: {
       type: String,
       required: true,
     },
-  },
-  ethnicity: {
-    type: String,
-    required: true,
-  },
-  guardian: {
-    name: {
+    religion: {
       type: String,
       required: true,
     },
-    relationship: {
+    birthplace: {
       type: String,
       required: true,
     },
+    address: {
+      street: {
+        type: String,
+        required: true,
+      },
+      barangay: {
+        type: String,
+        required: true,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+    },
+    ethnicity: {
+      type: String,
+      required: true,
+    },
+    guardian: {
+      name: {
+        type: String,
+        required: true,
+      },
+      relationship: {
+        type: String,
+        required: true,
+      },
+    },
+    physician: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: User,
+    },
   },
-  physician: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: User,
-  },
-
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  }
-});
+  { timestamps: true }
+);
 
 const Patient = mongoose.model("Patient", PatientSchema, "patient");
 module.exports = Patient;
