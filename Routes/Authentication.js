d30cda477be2d158636c78a031b7d01a8aa37ba1
Arzl James <70579069+arzljames@users.noreply.github.com>
2022-08-02 10:37:45 +0800

    sds

--- Routes/Authentication.js
+++ Routes/Authentication.js
@@ -53,7 +53,7 @@ router.post("/register", async (req, res) => {
       User.create(user, (err, result) => {
         if (err) {
           err.message.includes("email")
-            ? res.send(err)
+            ? res.send({ emailErr: err })
             : res.send({ usernameErr: "Username is already in use" });
         } else {
           Facilities.findOneAndUpdate(
